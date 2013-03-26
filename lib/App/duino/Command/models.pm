d0357f6ccbcd39992d4b1ae0be6d25f5c58abcb7
Alessandro Ghedini <alessandro@ghedini.me>
2013-03-26 12:27:19 +0100

    models.pm: add 'models' command

---
+++ lib/App/duino/Command/models.pm
@@ -0,0 +1,64 @@
+package App::duino::Command::models;
+
+use strict;
+use warnings;
+
+use App::duino -command;
+
+=head1 NAME
+
+App::duino::Command::models - List all known Arduino models
+
+=head1 SYNOPSIS
+
+  $ duino models
+
+=cut
+
+sub abstract { 'list all known Arduino models' }
+
+sub usage_desc { '%c models' }
+
+sub execute {
+	my ($self, $opt, $args) = @_;
+
+	my $boards = $self -> file($opt, 'hardware/arduino/boards.txt');
+
+	open my $fh, '<', $boards
+		or die "Can't open file 'boards.txt'.\n";
+
+	while (my $line = <$fh>) {
+		chomp $line;
+
+		my $first = substr $line, 0, 1;
+
+		next if $first eq '#' or $first eq '';
+		next unless $line =~ /^(.*)\.name\=/;
+
+		my $board = $1;
+
+		my (undef, $value) = split '=', $line;
+
+		printf "%15s: %s\n", $board, $value;
+	}
+
+	close $fh;
+}
+
+=head1 AUTHOR
+
+Alessandro Ghedini <alexbio@cpan.org>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Alessandro Ghedini.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1; # End of App::duino::Command::models
